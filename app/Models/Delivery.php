3142e283594c68b14f9b74b36c18ea317867f1cb
mihsoldi <srlsoldi3@gmail.com>
2022-01-03 11:54:00 +0200

    Modify up a new Component interface Entity

--- app/Models/Delivery.php
+++ app/Models/Delivery.php
@@ -25,3 +25,46 @@ class Delivery extends Model
         return $this->hasMany(CountryDelivery::class, 'delivery_id');
     }
 }
+
+>load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
+
+ =>  url($this->lang->lang.'/order/payment/fail/'.self::$order->id),
+                        "resultUrl" => URL::route('paydo-success', ['orderId' => self::$order->id, 'payment' => self::$payment]),
+                        "failPath" =>  URL::route('paydo-fail',    ['orderId' => self::$order->id, 'payment' => self::$payment]),
+                    ],
+            ]);
+
+            $invoceId = json_decode($request->getBody()->getContents());
+            return redirect('https://paydo.com/en/payment/invoice-preprocessing/'.$invoceId->data);
+        } catch (\Exception $e) {
+            $problem = "Payment Paydo error.";
+            LogsHandler::save(debug_backtrace(), $problem, \Auth::guard('persons')->user());
+        }
+    }
+
+    public function getSuccessStatus($orderId, $payment)
+    {
+        self::$order = CRMOrders::find($orderId);
+        self::$payment = $payment;
+
+        $this->success();
+        return redirect()->route('thanks', ['redirs' => 'success', 'checkout' => self::$order->id, 'promocode' => self::$promocode->id]);
+    }
+
+    public function getFailStatus($orderId, $payment)
+    {
+        self::$order = CRMOrders::find($orderId);
+        self::$payment = $payment;
+
+        $this->fail();
+    }
+
+    public function callBack()
+    {
+        // code...
+    }
+}
--- database/migrations/2019_01_30_122353_create_autoalts.php
+++ database/migrations/2019_01_30_122353_create_autoalts.php
@@ -36,3 +36,30 @@ class CreateAutoalts extends Migration
         Schema::dropIfExists('autoalts');
     }
 }
+
+         $table->unsignedInteger('blog_category_id')->nullable();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->text('banner_desktop')->nullable();
+            $table->text('banner_mobile')->nullable();
+            $table->text('seo_text')->nullable();
+            $table->string('seo_title')->nullable();
+            $table->text('seo_description')->nullable();
+            $table->text('seo_keywords')->nullable();
+            $table->timestamps();
+
+            $table->foreign('lang_id')->references('id')->on('langs')->onDelete('cascade');
+            $table->foreign('blog_category_id')->references('id')->on('blog_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_categories_translation');
+    }
+}
