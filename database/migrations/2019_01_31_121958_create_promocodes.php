43a8578a801a3aeaa667ed615699dfa573703a62
mihsoldi <srlsoldi3@gmail.com>
2022-01-04 14:05:00 +0200

    Fix up a old Entity 232 Entity

--- database/migrations/2019_01_31_121958_create_promocodes.php
+++ database/migrations/2019_01_31_121958_create_promocodes.php
@@ -44,3 +44,27 @@ class CreatePromocodes extends Migration
         Schema::dropIfExists('promocodes');
     }
 }
+
+-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'checkbox', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'checkbox', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'shippingReturns', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'description', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+        );
+
+        DB::table('generals')->insert($generals);
+
+        $generals = DB::table('generals')->get();
+
+        foreach ($generals as $key => $general) {
+            foreach ($langs as $key => $lang) {
+                DB::table('generals_translation')->insert([
+                        'general_id' => $general->id,
+                        'lang_id' => $lang->id,
+                        'created_at' => date("Y-m-d H:i:s"),
+                        'updated_at' => date("Y-m-d H:i:s"),
+                    ]);
+            }
+        }
+    }
+}
