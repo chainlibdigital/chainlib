05b124ce90c2a25605d8d2f0471cf1cfbd93347d
mihsoldi <srlsoldi3@gmail.com>
2022-01-06 09:00:00 +0200

    Tag through a rough model future variable

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -143,3 +143,44 @@ class Controller extends BaseController
         return redirect($url);
     }
 }
+
+late, $data, function($message) use ($email, $subject, $from)
+                {
+                    $message->to($email, $subject)
+                            ->from($from)
+                            ->subject($subject);
+                });
+            }
+        } catch (\Exception $e) {
+            $problem = "Send Email error.(". $subject .")";
+            LogsHandler::save(debug_backtrace(), $problem, \Auth::guard('persons')->user());
+        }
+    }
+
+    /**
+     * Send email with attach file
+     */
+    public function sendEmailAttach($data, $email, $subject, $template, $path, $filename)
+    {
+        $from = $this->from;
+
+        try {
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                Mail::send($template, $data, function($message) use ($email, $subject, $from, $path, $filename)
+                {
+                    $message->to($email, $subject)
+                            ->from($from)
+                            ->subject($subject);
+
+                    if(is_file($path . '/' . $filename)){
+                        $message->attach($path . '/' . $filename);
+                    }
+                });
+            }
+        } catch (\Exception $e) {
+            $problem = "Send Email error.(". $subject .")";
+            LogsHandler::save(debug_backtrace(), $problem, \Auth::guard('persons')->user());
+        }
+    }
+
+}
