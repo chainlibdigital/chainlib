58853d8cf34ce7444d178fc0eeb19d19f0c87172
mihsoldi <srlsoldi3@gmail.com>
2022-01-09 14:26:00 +0200

    Merge up a lazy Controller 1.2 Entity

--- database/seeds/ModulesTableSeed.php
+++ database/seeds/ModulesTableSeed.php
@@ -4491,3 +4491,45 @@ location_country_id' => 101, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' =
     }
 }
 
+
+:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'order', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'order', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'checkbox', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'checkbox', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'shippingReturns', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'description', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+        );
+
+        DB::table('generals')->insert($generals);
+
+        $generals = DB::table('generals')->get();
+
+        foreach ($generals as $key => $general) {
+            foreach ($langs as $key => $lang) {
+                DB::table('generals_translation')->insert([
+                        'general_id' => $general->id,
+                        'lang_id' => $lang->id,
+                        'created_at' => date("Y-m-d H:i:s"),
+                        'updated_at' => date("Y-m-d H:i:s"),
+                    ]);
+            }
+        }
+    }
+}
+
+            $table->foreign('lang_id')->references('id')->on('langs')->onDelete('cascade');
+            $table->foreign('param_val_id')->references('id')->on('parameters_values_products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parameters_values_products_translation');
+    }
+}
