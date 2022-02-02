8c804ac0524cc30146499e518600a896c4b4189f
mihsoldi <srlsoldi3@gmail.com>
2022-02-02 11:06:00 +0200

    Change down a old Entity 12.3 Component

--- resources/views/front/partials/header.blade.php
+++ resources/views/front/partials/header.blade.php
@@ -185,3 +185,70 @@
         </div>
     </div>
 </header>
+
+              @if ($resource->children()->count() > 0)
+                                @foreach ($resource->children as $key => $child)
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ $key == 0 ? 'active' : '' }}" data-toggle="pill" href="#menu{{ $child->id }}">
+                                        {{ $child->translation->name }}</a>
+                                    </li>
+                                @endforeach
+                            @endif
+                        </ul>
+                    </div>
+                    <div class="col-md-9 tab-content category-one-item">
+                      @if ($resource->children()->count() > 0)
+                          @foreach ($resource->children as $key => $child)
+                          <div id="menu{{ $child->id }}" class="tab-section tab-pane fade {{ $key == 0 ? 'active in show' : '' }} ">
+
+                              <div class="card card-body">
+                                <div class="row">
+                                  <div class="col-md-8 col-12">
+                                    @if ($child->translation->description)
+                                        <p>{{ $child->translation->description }} </p>
+                                    @endif
+                                    <ul>
+                                        @foreach ($child->accordions as $key => $accordion)
+                                        <li>
+                                            <a href="{{ $accordion->translation->link }}" target="_blank">
+                                            {{ $accordion->translation->title }}
+                                            </a>
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                  </div>
+                                  <div class="col-md-4 col-12">
+                                        <div class="image-static">
+                                          @if ($child->image_right)
+                                            <div class="image-container">
+                                                <a href="{{ $child->link }}" target="_blank">
+                                                    <img src="/images/blogCategories/og/{{ $child->image_right }}" alt="">
+                                                </a>
+                                            </div>
+                                          @endif
+                                          <div class="sharing_block">
+                                              <div class="addthis_toolbox addthis_default_style ">
+                                                <a class="addthis_button_preferred_1"></a>
+                                                <a class="addthis_button_preferred_2"></a>
+                                                <a class="addthis_button_preferred_3"></a>
+                                                <a class="addthis_button_preferred_4"></a>
+                                                <a class="addthis_button_compact"></a>
+                                                <a class="addthis_counter addthis_bubble_style"></a>
+                                              </div>
+                                              <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=xa-507bec157716424a"></script>
+                                          </div>
+                                        </div>
+                                  </div>
+                                </div>
+                              </div>
+                          </div>
+                          @endforeach
+                      @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+@include('front.partials.footer')
+@stop
--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -226,3 +226,22 @@ rg/2000/svg"
         @endif
     </div>
 @endif
+
+                     @endif
+                         </a>
+                         {{-- <a href="{{ url('/'.$lang->lang.'/catalog/'. $child->alias) }}"> --}}
+                             <a class="title" href="{{ url('/'.$lang->lang.'/catalog/'. $child->alias) }}">
+                                 {{ $child->translation->name }}
+                             </a>
+                         {{-- </a> --}}
+                       </div>
+                     </div>
+                 @endforeach
+             @endif
+
+         </div>
+       </div>
+     </main>
+
+@include('front.partials.footer')
+@stop
