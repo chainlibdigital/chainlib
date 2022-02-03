9d3613176dc598f5929ef8275f10de50bbe4547c
mihsoldi <srlsoldi3@gmail.com>
2022-02-03 12:15:00 +0200

    Change below a lazy Controller future model

--- app/Console/Commands/updateStocks.php
+++ app/Console/Commands/updateStocks.php
@@ -43,3 +43,371 @@ class updateStocks extends Command
     }
 }
 
+
+ $allLangs = DB::table('lang')->get();
+
+    $urlLang = "";
+
+    // if ($lang == $defaultLang->lang) {
+    //
+    //     dd(request()->url());
+    // }else{
+    //
+    // }
+
+    $url = $urlLang;
+
+    if (request()->segment(2)) {
+        $url = $urlLang.'/'.request()->segment(1).'/'.request()->segment(2);
+    }elseif (request()->segment(3)) {
+        $url = $urlLang.'/'.request()->segment(1).'/'.request()->segment(2).'/'.request()->segment(3);
+    }elseif (request()->segment(4)) {
+        $url = $urlLang.'/'.request()->segment(1).'/'.request()->segment(2).'/'.request()->segment(3).'/'.request()->segment(4);
+    }
+
+    return $url;
+}
+
+
+function isJson($string) {
+     json_decode($string);
+     return (json_last_error() == JSON_ERROR_NONE);
+}
+
+
+/**
+ * @param $lang_id
+ * @param $id
+ * @param null $curr_id
+ * @return string
+ */
+function SelectGoodsCatsTree($lang_id, $id, $curr_id = null, $level = 0)
+{
+    $menu_id_by_level = DB::table('categories')
+        ->where('parent_id', $id)
+        ->orderBy('position', 'asc')
+        ->get();
+
+    $menu_by_level = [];
+    foreach ($menu_id_by_level as $key => $one_menu_id_by_level) {
+        $menu_by_level[$key] = DB::table('categories_translation')
+            ->join('categories', 'categories_translation.category_id', '=', 'categories.id')
+            ->where('category_id', $one_menu_id_by_level->id)
+            ->where('lang_id', $lang_id)
+            ->first();
+    }
+
+    $result = array();
+
+    $menu_by_level = array_filter($menu_by_level);
+    $level++;
+
+    if (sizeof($menu_by_level) > 0) {
+        $result[] = '<ol class="dd-list">';
+        foreach ($menu_by_level as $entry) {
+
+            $edit = route('categories.edit', $entry->category_id);
+            $delete = route('categories.destroy', $entry->category_id);
+
+            if ((!checkPosts($entry->id)) && ($level != 4)) {
+                $addNew = '#addCategory';
+                $postsLink = '';
+            } else {
+                $addNew = '#warning';
+                $postsLink = '<a href="/back/posts/category/' . $entry->category_id . '"><i class="fa fa-bars"></i></a>';
+            }
+
+            $result[] = sprintf(
+                '<li class="dd-item dd3-item" data-id="' . $entry->category_id . '">
+                %s
+                <div class="dd-handle dd3-handle">
+                <i class="fa fa-bars"></i>
+                </div><div class="dd3-content">
+                </div>
+                %s
+            </li>',
+                '<span>' . $entry->name . '</span><div class="buttons">
+
+              ' . $postsLink . '
+
+               <a href="' . $edit . '"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+
+               <a href="/back/posts/category/' . $entry->category_id . '"><i class="fa fa-eye" aria-hidden="true"></i></a>
+
+               <a class="btn-link modal-id" data-toggle="modal" data-target="' . $addNew . '" data-id="' . $entry->category_id . '" data-name="' . $entry->name . '">
+               <i class="fa fa-plus" aria-hidden="true"></i>
+               </a>
+
+               <form method="post" action=" ' . $delete . '">
+                 ' . csrf_field() . method_field("DELETE") . '
+               <button type="submit" class="btn-link"><a class="modal-id" data-toggle="modal" data-target="' . $addNew . '_delete" data-id="' . $entry->category_id . '" data-name="' . $entry->name . '" href=""><i class="fa fa-trash" aria-hidden="true"></i></a></button>
+               </form>
+
+           </div>',
+
+                SelectGoodsCatsTree($lang_id, $entry->category_id, 0, $level)
+            );
+        }
+        $result[] = '</ol>';
+    }
+
+    return implode($result);
+}
+
+function hasSubmodule($id)
+{
+  $table = "modules";
+
+  $row = DB::table($table)
+      ->where('parent_id', $id)
+      ->get();
+
+  return $row;
+}
+
+function moduleItems($table)
+{
+    if (Schema::hasTable($table)) {
+        $row = DB::table($table)
+            ->select('id')
+            ->get();
+
+        return $row->count();
+    }
+    return false;
+
+}
+
+/**
+ * @param $lang_id
+ * @param $id
+ * @return string
+ */
+function SelectProdsCatsTree($lang_id, $id)
+{
+    $categories = DB::table('product_categories_translation')
+        ->join('product_categories', 'product_categories_translation.product_category_id', '=', 'product_categories.id')
+        ->where('parent_id', $id)
+        ->where('lang_id', $lang_id)
+        ->orderBy('position', 'asc')
+        ->get();
+
+    return $categories ?? null;
+}
+
+/**
+ * @param $lang_id
+ * @param $id
+ * @param null $curr_id
+ * @return string
+ */
+function SelectMenusTree($lang_id, $id, $curr_id = null, $level = 0, $groupId)
+{
+    $menu_id_by_level = DB::table('menus')
+        ->where('group_id', $groupId)
+        ->where('parent_id', $id)
+        ->orderBy('position', 'asc')
+        ->get();
+
+
+    $menu_by_level = [];
+    foreach ($menu_id_by_level as $key => $one_menu_id_by_level) {
+        $menu_by_level[$key] = DB::table('menus_translation')
+            ->join('menus', 'menus_translation.menu_id', '=', 'menus.id')
+            ->where('menu_id', $one_menu_id_by_level->id)
+            ->where('lang_id', $lang_id)
+            ->first();
+    }
+
+    $result = array();
+
+    $menu_by_level = array_filter($menu_by_level);
+    $level++;
+
+    if (sizeof($menu_by_level) > 0) {
+        $result[] = '<ol class="dd-list">';
+        foreach ($menu_by_level as $entry) {
+
+            $edit = route('menus.edit', $entry->menu_id);
+            $delete = route('menus.destroy', $entry->menu_id);
+
+            if ((!checkPosts($entry->id)) && ($level != 4)) {
+                $addNew = '#addCategory';
+            } else {
+                $addNew = '#warning';
+            }
+
+            $result[] = sprintf(
+                '<li class="dd-item dd3-item" data-id="' . $entry->menu_id . '">
+                %s
+                <div class="dd-handle dd3-handle">
+                <i class="fa fa-bars"></i>
+                </div><div class="dd3-content">
+                </div>
+                %s
+            </li>',
+                '<span>' . $entry->name .' - ' . $entry->url. '</span><div class="buttons">
+
+               <a href="' . $edit . '"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+
+               <a href=""><i class="fa fa-eye" aria-hidden="true"></i></a>
+
+               <a class="btn-link modal-id" data-toggle="modal" data-target="' . $addNew . '" data-id="' . $entry->menu_id . '" data-name="' . $entry->name . '">
+               <i class="fa fa-plus" aria-hidden="true"></i>
+               </a>
+
+               <form method="post" action=" ' . $delete . '">
+                 ' . csrf_field() . method_field("DELETE") . '
+               <button type="submit" class="btn-link"><a class="modal-id" data-toggle="modal" data-target="' . $addNew . '_delete" data-id="' . $entry->menu_id . '" data-name="' . $entry->name . '" href=""><i class="fa fa-trash" aria-hidden="true"></i></a></button>
+               </form>
+
+           </div>',
+
+                SelectMenusTree($lang_id, $entry->menu_id, 0, $level, $groupId)
+            );
+        }
+        $result[] = '</ol>';
+    }
+
+    return implode($result);
+}
+
+/**
+ * @param $lang_id
+ * @param $id
+ * @param null $curr_id
+ * @return string
+ */
+function SelectProductCategoriesTree($lang_id, $id, $curr_id = null, $level = 0)
+{
+    $menu_id_by_level = DB::table('product_categories')
+        ->where('parent_id', $id)
+        ->orderBy('position', 'asc')
+        ->get();
+
+    $menu_by_level = [];
+    foreach ($menu_id_by_level as $key => $one_menu_id_by_level) {
+        $menu_by_level[$key] = DB::table('product_categories_translation')
+            ->join('product_categories', 'product_categories_translation.product_category_id', '=', 'product_categories.id')
+            ->where('product_category_id', $one_menu_id_by_level->id)
+            ->where('lang_id', $lang_id)
+            ->first();
+    }
+
+    $result = array();
+
+    $menu_by_level = array_filter($menu_by_level);
+    $level++;
+
+    if (sizeof($menu_by_level) > 0) {
+        $result[] = '<ol class="dd-list">';
+        foreach ($menu_by_level as $entry) {
+
+            $edit = route('product-categories.edit', $entry->product_category_id);
+            $delete = route('product-categories.destroy', $entry->product_category_id);
+
+            if ((!checkProducts($entry->id)) && ($level != 4)) {
+                $addNew = '#addCategory';
+                $postsLink = '';
+            } else {
+                $addNew = '#warning';
+                $postsLink = '<a href="/back/products/category/' . $entry->product_category_id . '"><i class="fa fa-bars"></i></a>';
+            }
+
+            $result[] = sprintf(
+                '<li class="dd-item dd3-item" data-id="' . $entry->product_category_id . '">
+                %s
+                <div class="dd-handle dd3-handle">
+                <i class="fa fa-bars"></i>
+                </div><div class="dd3-content">
+                </div>
+                %s
+            </li>',
+                '<span>' . $entry->name .'  </span><div class="buttons">
+                '. $postsLink .'
+               <a href="' . $edit . '"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+
+               <a class="btn-link modal-id" data-toggle="modal" data-target="' . $addNew . '" data-id="' . $entry->product_category_id . '" data-name="' . $entry->name . '">
+               <i class="fa fa-plus" aria-hidden="true"></i>
+               </a>
+
+               <form method="post" action=" ' . $delete . '">
+                 ' . csrf_field() . method_field("DELETE") . '
+               <button type="submit" class="btn-link"><a class="modal-id" data-toggle="modal" data-target="' . $addNew . '_delete" data-id="' . $entry->product_category_id . '" data-name="' . $entry->name . '" href=""><i class="fa fa-trash" aria-hidden="true"></i></a></button>
+               </form>
+
+           </div>',
+
+                SelectProductCategoriesTree($lang_id, $entry->product_category_id, 0, $level)
+            );
+        }
+        $result[] = '</ol>';
+    }
+
+    return implode($result);
+}
+
+
+/**
+ * @param $lang_id
+ * @param $id
+ * @return string
+ */
+function SelectCatsTree($lang_id, $id)
+{
+    $categories = DB::table('categories_translation')
+        ->join('categories', 'categories_translation.category_id', '=', 'categories.id')
+        ->where('parent_id', $id)
+        ->where('lang_id', $lang_id)
+        ->get();
+
+    return $categories ?? null;
+}
+
+// function checkPosts($id)
+// {
+//     $row = DB::table('posts')
+//         ->where('category_id', $id)
+//         ->first();
+//
+//     if (!is_null($row)) {
+//         return true;
+//     }
+//     return false;
+// }
+
+
+// function checkPropertyCat($category_id, $property_id){
+//     $row = DB::table('property_categories')
+//         ->where('property_id', $property_id)
+//         ->where('category_id', $category_id)
+//         ->first();
+//
+//     if (!is_null($row)) {
+//         return true;
+//     }
+//     return false;
+// }
+
+// function checkPropertyCatGroup($category_id, $group_id){
+//     $properties = DB::table('product_properties')
+//         ->select('id')
+//         ->where('group_id', $group_id)
+//         ->get();
+//
+//     if (!empty($properties)) {
+//         foreach ($properties as $key => $property) {
+//             $row = DB::table('property_categories')
+//                 ->select('id')
+//                 ->where('property_id', $property->id)
+//                 ->where('category_id', $category_id)
+//                 ->first();
+//
+//             if (is_null($row)) {
+//                 return false;
+//             }
+//         }
+//     }
+//
+//     return true;
+// }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,12 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collections_translation');
+    }
+}
