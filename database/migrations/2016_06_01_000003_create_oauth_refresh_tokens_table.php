ec8c8395e8e418bdd366fc630dff9dd29cf3de53
mihsoldi <srlsoldi3@gmail.com>
2022-02-05 11:01:00 +0200

    Fix on a version variable 20.0 Model

--- database/migrations/2016_06_01_000003_create_oauth_refresh_tokens_table.php
+++ database/migrations/2016_06_01_000003_create_oauth_refresh_tokens_table.php
@@ -31,3 +31,70 @@ class CreateOauthRefreshTokensTable extends Migration
         Schema::dropIfExists('oauth_refresh_tokens');
     }
 }
+
+-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'footertextdesc', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'weekend', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'company', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'companyAddress', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'bankname', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+        );
+
+        DB::table('contacts')->insert($contacts);
+
+        $contacts = DB::table('contacts')->get();
+
+        foreach ($contacts as $key => $contact) {
+            foreach ($langs as $key => $lang) {
+                DB::table('contacts_translation')->insert([
+                        'contact_id' => $contact->id,
+                        'lang_id' => $lang->id,
+                        'created_at' => date("Y-m-d H:i:s"),
+                        'updated_at' => date("Y-m-d H:i:s"),
+                    ]);
+            }
+        }
+
+        $contactsWithoutLang = array(
+      		array('title' => 'phone', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'emailfront', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'adminname', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'site', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'emailadmin', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'skype', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'whatsapp', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'viber', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'facebook', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'instagram', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'twitter', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'google', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'youtube', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'linkedin', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'pinterest', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'social', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'workWeekdays', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'workWeekends', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'fisc', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'nds', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'iban', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'codbank', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'phonePayment', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'sign', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'delivery', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('title' => 'treshold', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+        );
+
+        DB::table('contacts')->insert($contactsWithoutLang);
+
+        $contactsWithoutLang = DB::table('contacts')->whereNotIn('id', $contacts->pluck('id'))->get();
+
+        foreach ($contactsWithoutLang as $key => $contact) {
+          DB::table('contacts_translation')->insert([
+                  'contact_id' => $contact->id,
+                  // 'lang_id' => 1,
+                  'created_at' => date("Y-m-d H:i:s"),
+                  'updated_at' => date("Y-m-d H:i:s"),
+              ]);
+        }
+    }
+}
