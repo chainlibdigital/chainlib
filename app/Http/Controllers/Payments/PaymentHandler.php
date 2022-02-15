26fa608b44e0544e1b204b30557d1c3688814022
mihsoldi <srlsoldi3@gmail.com>
2022-02-15 15:57:00 +0200

    Merge on a version variable 20.0 script

--- app/Http/Controllers/Payments/PaymentHandler.php
+++ app/Http/Controllers/Payments/PaymentHandler.php
@@ -208,3 +208,88 @@ class PaymentHandler extends Controller
         return self::$promocode = null;
     }
 }
+
+    foreach (Lang::all() as $lang) {
+                foreach ($staticPages as $staticPage) {
+                    $aSiteMap[$url.'/'.$lang->lang.'/'. $siteType . '/' .$staticPage] = [
+                        'added' => time(),
+                        'lastmod' => Carbon::now()->toIso8601String(),
+                        'priority' => 1 - substr_count($url.'/'.$lang->lang.'/'.$staticPage, '/') / 10,
+                        'changefreq' => 'always'
+                    ];
+                }
+            }
+        }
+
+        foreach (Lang::all() as $lang) {
+            if(count($pages) > 0) {
+                foreach ($pages as $page) {
+                    $aSiteMap[$url.'/'.$lang->lang.'/'. $siteType . '/'. $page->alias] = [
+                        'added' => time(),
+                        'lastmod' => Carbon::now()->toIso8601String(),
+                        'priority' => 1 - substr_count($url.'/'.$page->alias, '/') / 10,
+                        'changefreq' => 'always'
+                    ];
+                }
+            }
+        }
+
+        foreach ($siteTypes as $key => $siteType) {
+            foreach (Lang::all() as $lang) {
+                if(count($categories) > 0) {
+                    foreach ($categories as $category) {
+                        if ($category->{$siteType} == 1) {
+                            $aSiteMap[$url.'/'.$lang->lang.'/'. $siteType . '/catalog/'.$category->alias] = [
+                                'added' => time(),
+                                'lastmod' => Carbon::now()->toIso8601String(),
+                                'priority' => 1 - substr_count($url.'/catalog/'.$category->alias, '/') / 10,
+                                'changefreq' => 'always'
+                            ];
+                        }
+                    }
+                }
+            }
+        }
+
+        foreach ($siteTypes as $key => $siteType) {
+            foreach (Lang::all() as $lang) {
+                if(count($products) > 0) {
+                    foreach ($products as $product) {
+                        if ($product->{$siteType} == 1 && $product->active == 1) {
+                            $aSiteMap[$url.'/'.$lang->lang.'/'. $siteType .'/catalog/'.$product->category->alias.'/'.$product->alias] = [
+                                'added' => time(),
+                                'lastmod' => Carbon::now()->toIso8601String(),
+                                'priority' => 1 - substr_count($url.'/catalog/'.$product->category->alias.'/'.$product->alias, '/') / 10,
+                                'changefreq' => 'always'
+                            ];
+                        }
+                    }
+                }
+            }
+        }
+
+        foreach ($siteTypes as $key => $siteType) {
+            foreach (Lang::all() as $lang) {
+                if(count($sets) > 0) {
+                    foreach ($sets as $set) {
+                        if ($set->{$siteType} == 1) {
+                            if ($set->collection) {
+                                $aSiteMap[$url.'/'.$lang->lang. '/' . $siteType. '/collection/'.$set->collection->alias] = [
+                                    'added' => time(),
+                                    'lastmod' => Carbon::now()->toIso8601String(),
+                                    'priority' => 1 - substr_count($url.'/collection/'.$set->collection->alias, '/') / 10,
+                                    'changefreq' => 'always'
+                                ];
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        \Cache::put('sitemap', $aSiteMap, 2880);
+
+        return Response::view('front.sitemap')->header('Content-Type', 'application/xml');
+    }
+}
