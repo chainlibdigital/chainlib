ed488523d4576e40e67ffcec23a92af6691d5d0d
mihsoldi <srlsoldi3@gmail.com>
2022-02-16 14:37:00 +0200

    Remove into a lazy Controller 232 Controller

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -564,3 +564,114 @@ RING);
         return array('is_logged' => 1, 'user_id' => auth('persons')->id(), 'status' => 'auth');
     }
 }
+
+ = Setup::$site;
+        $this->device = Setup::$device;
+
+//        $this->warehouse = Setup::$warehouse->name;
+        $this->mainCurrency = Setup::$mainCurrency;
+        $this->currency = Setup::$currency;
+
+        $this->country = Setup::$country;
+        $this->userId = Setup::$userId;
+
+    }
+
+    public function saveSettings(Request $request)
+    {
+        $url = str_replace(url('/'), '', url()->previous());
+        $previousLang = $this->lang->lang;
+
+        $country = Country::where('id', $request->get('countryId'))->first();
+        $lang   = Lang::where('id', $request->get('langId'))->first();
+        $currency = Currency::where('id', $request->get('currencyId'))->first();
+
+        if (!is_null($country)) {
+            setcookie('country_id', $country->id, time() + 10000000, '/');
+            setcookie('currency_id', $currency->id, time() + 10000000, '/');
+        }
+
+        if (!is_null($lang)) {
+            setcookie('lang_id', $lang->lang, time() + 10000000, '/');
+            $url =  str_replace('/'.$previousLang, '/'.$lang->lang, $url);
+        }
+
+        return url($url);
+    }
+
+    public function getCurrency(Request $request)
+    {
+        $currency = Currency::where('id', $request->get('currencyId'))->first();
+
+        if (!is_null($currency)) {
+            return $currency;
+        }
+        return "false";
+    }
+
+    public function getCountry(Request $request)
+    {
+        $country = Country::where('id', $request->get('countryId'))->first();
+
+        if (!is_null($country)) {
+            return $country;
+        }
+        return "false";
+    }
+
+    public function saveCountryUser(Request $request)
+    {
+        $country = Country::where('id', $request->get('countryId'))->first();
+        $lang = Lang::where('id', $request->get('langId'))->first();
+        $currency = Currency::where('id', $request->get('currencyId'))->first();
+
+        if (!is_null($country)) {
+            setcookie('country_id', $country->id, time() + 10000000, '/');
+            setcookie('lang_id', $lang->lang, time() + 10000000, '/');
+            setcookie('currency_id', $currency->id, time() + 10000000, '/');
+        }
+
+        return $lang->lang;
+    }
+
+    public function acceptCookiePolicy()
+    {
+        setcookie('cookie_policy', true, time() + 10000000, '/');
+    }
+
+    public function setUserSettings(Request $request)
+    {
+        $url = str_replace(url('/'), '', url()->previous());
+        $previousLang = $this->lang->lang;
+
+        $country = Country::where('id', $request->get('country_id'))->first();
+        $lang = Lang::where('id', $request->get('lang_id'))->first();
+        $currency = Currency::where('id', $request->get('currency_id'))->first();
+
+        if (!is_null($country)) {
+            setcookie('country_id', $country->id, time() + 10000000, '/');
+            setcookie('warehouse_id', $country->warehouse_id, time() + 10000000, '/');
+        }
+
+        if (!is_null($lang)) {
+            setcookie('lang_id', $lang->lang, time() + 10000000, '/');
+            $url =  str_replace('/'.$previousLang, '/'.$lang->lang, $url);
+        }
+
+        if ($country->iso == "MD") {
+            if (!is_null($currency)) {
+                setcookie('currency_id', $country->currency_id, time() + 10000000, '/');
+            }
+        }else{
+            if (!is_null($currency)) {
+                if ($currency->abbr == "MDL") {
+                    $currency = Currency::where('type', 1)->first();
+                    setcookie('currency_id', $currency->id, time() + 10000000, '/');
+                }else{
+                    setcookie('currency_id', $currency->id, time() + 10000000, '/');
+                }
+            }
+        }
+        return redirect($url);
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -467,3 +467,409 @@ me' => $faker->name,
         Schema::dropIfExists('returns');
     }
 }
+
+n_register' => 1,
+            'in_cabinet' => 1,
+            'in_cart' => 1,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 1,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'email',
+            'type' => 'email',
+            'field_group' => 'personaldata',
+            'in_register' => 1,
+            'in_cabinet' => 1,
+            'in_cart' => 1,
+            'in_auth' => 1,
+            'unique_field' => 1,
+            'required_field' => 1,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'phone',
+            'type' => 'text',
+            'field_group' => 'personaldata',
+            'in_register' => 1,
+            'in_cabinet' => 1,
+            'in_cart' => 1,
+            'in_auth' => 0,
+            'unique_field' => 1,
+            'required_field' => 1,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'birthday',
+            'type' => 'date',
+            'field_group' => 'personaldata',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'terms_agreement',
+            'type' => 'checkbox',
+            'field_group' => 'personaldata',
+            'in_register' => 1,
+            'in_cabinet' => 1,
+            'in_cart' => 1,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 1,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'promo_agreement',
+            'type' => 'checkbox',
+            'field_group' => 'personaldata',
+            'in_register' => 1,
+            'in_cabinet' => 1,
+            'in_cart' => 1,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'personaldata_agreement',
+            'type' => 'checkbox',
+            'field_group' => 'personaldata',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'addressname',
+            'type' => 'text',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 1,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 1,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'country',
+            'type' => 'select',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 1,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 1,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'region',
+            'type' => 'select',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 1,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 1,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'location',
+            'type' => 'select',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 1,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 1,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'address',
+            'type' => 'text',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 1,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'code',
+            'type' => 'text',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'apartment',
+            'type' => 'text',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'company',
+            'type' => 'text',
+            'field_group' => 'company',
+            'in_register' => 0,
+            'in_cabinet' => 0,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'companyaddress',
+            'type' => 'text',
+            'field_group' => 'company',
+            'in_register' => 0,
+            'in_cabinet' => 0,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'fisc',
+            'type' => 'text',
+            'field_group' => 'company',
+            'in_register' => 0,
+            'in_cabinet' => 0,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'priorityaddress',
+            'type' => 'select',
+            'field_group' => 'priorityaddress',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'homenumber',
+            'type' => 'text',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'entrance',
+            'type' => 'text',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'floor',
+            'type' => 'text',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'comment',
+            'type' => 'text',
+            'field_group' => 'address',
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'maxaddress',
+            'type' => 'select',
+            'field_group' => 'general',
+            'value' => '10',
+            'in_register' => 0,
+            'in_cabinet' => 0,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'countries',
+            'type' => 'select',
+            'field_group' => 'general',
+            'in_register' => 0,
+            'in_cabinet' => 0,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('userfields')->insert([
+            'field' => 'return_amount_days',
+            'type' => 'text',
+            'field_group' => 'general',
+            'value' => 6,
+            'in_register' => 0,
+            'in_cabinet' => 1,
+            'in_cart' => 0,
+            'in_auth' => 0,
+            'unique_field' => 0,
+            'required_field' => 0,
+            'return_field' => 0,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+    }
+}
+
+
+);
+            $table->string('code')->nullable();
+            $table->string('video')->nullable();
+            $table->unsignedInteger('stock')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->decimal('actual_price', 10, 2)->nullable();
+            $table->string('discount')->nullable();
+            $table->string('discount_update')->nullable();
+            $table->tinyInteger('hit')->nullable();
+            $table->tinyInteger('recomended')->nullable();
+            $table->unsignedInteger('position')->nullable();
+            $table->unsignedInteger('succesion')->nullable();
+            $table->tinyInteger('active')->default(1);
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('product_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
