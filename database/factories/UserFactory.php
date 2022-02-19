f973d84283c25480f0d021f0b04ab61041aef2cf
mihsoldi <srlsoldi3@gmail.com>
2022-02-19 16:11:00 +0200

    Remove into a max feature interface Controller

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1309,3 +1309,80 @@ ng('region')->nullable();
     }
 }
 
+
+t' => date("Y-m-d H:i:s"), ),
+          array('name' => 'order', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'order', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'checkbox', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'checkbox', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'shippingReturns', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+          array('name' => 'description', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s"), ),
+        );
+
+        DB::table('generals')->insert($generals);
+
+        $generals = DB::table('generals')->get();
+
+        foreach ($generals as $key => $general) {
+            foreach ($langs as $key => $lang) {
+                DB::table('generals_translation')->insert([
+                        'general_id' => $general->id,
+                        'lang_id' => $lang->id,
+                        'created_at' => date("Y-m-d H:i:s"),
+                        'updated_at' => date("Y-m-d H:i:s"),
+                    ]);
+            }
+        }
+    }
+}
+
+tring('treshold')->nullable();
+            $table->string('period')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promocode_types');
+    }
+}
+
+     'remember_token' => str_random(10),
+    ];
+});
+
+');
+    ];
+});
+
+
+    public function down()
+    {
+        Schema::dropIfExists('oauth_refresh_tokens');
+    }
+}
+
+
+;
+            $table->text('motive')->nullable();
+            $table->dateTime('datetime')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('returns');
+    }
+}
